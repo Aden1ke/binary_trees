68bd3fec94243cb3bde71c7e1f422122520234d6
Aden1ke <sarahogunlalu@.com>
2023-11-03 00:51:56 +0100

    debugging a semi colon

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,7 +6,7 @@
  * @value: the value stored
  * Return: node.
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
