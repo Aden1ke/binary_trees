97751309c1c4f7c75c60f7256437c9dcdf5d28db
Aden1ke <sarahogunlalu@.com>
2023-11-03 21:59:34 +0100

    binary

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
- * binary_tree_height - Measures the height of a binary tree
+ * binary_tree_height - Measures the height of avtree
  * @tree: Pointer to the root node of the tree
- * Return: The height of the tree starting at @tree
+ * Return: The height of the tree
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
